ff9ea1d35d4fb97e98e9ccc8f695b643c9cd2732
Balakrishnakollala <82933407+Balakrishnakollala@users.noreply.github.com>
2021-05-03 16:03:02 -1200

    bala krishna  k

---
+++ TestBase.java
@@ -0,0 +1,30 @@
+package com.Alibabapackage;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class TestBase {
+
+	WebDriver driver;
+
+	Alibabapages alibaba;
+	Actions action;
+
+	@BeforeClass
+	@Parameters({ "Url" })
+	public void setUp(String Url) {
+		System.setProperty("webdriver.chrome.driver", "F:\\virtusa softwares\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		action = new Actions(driver);
+		alibaba = new Alibabapages(driver);
+		driver.get(Url);
+
+	}
+	
+
+}
